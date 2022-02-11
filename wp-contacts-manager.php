7f2c24444a4ef52d97d8680672ee46ce9246db42
Naym Mupoia <medvanio4@gmail.com>
2022-02-11 01:06:52 +0100

    Update Wp Contacts Manager file

---
+++ wp-contacts-manager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *   Plugin Name: Wordpress plugin to manage contacts
+ *   Plugin URI: #
+ *   Description: O plug-in do WordPress para gerenciar contatos é um plug-in do WordPress para gerenciar contatos, tudo a partir do painel do WP. O WP Contacts Manager é um CRUD: Crie, leia, atualize e exclua. 
+ *   Version: 1.0.0
+ *   Author: Naym Mupoia
+ *   Author URI: https://www.facebook.com/irvanio.braga/
+ *   License: GPL-2.0+
+ *   License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *   Text Domain: wp_contacts_manager
+ *   Domain Path: /languages/
+ */
+/**
+ *   @package    Wordpress plugin to manage contacts
+ *   @author     Naym Mupoia
+ */
+
+?>
